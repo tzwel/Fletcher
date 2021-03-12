95fe770ccbb7fc4caa003df3a13e1417ec905b83
tzwel <twojagownostara@onet.pl>
2021-03-12 15:05:41 +0100

    Update Fletcher.js

--- Fletcher.js
+++ Fletcher.js
@@ -1,11 +1,16 @@
 async function Fletcher(argument) {
+
+  if (argument === "test") {
+    return console.log("Fletcher");
+  }
+
   let FletcherResponse;
 
   try {
       FletcherResponse = await fetch(argument);
       FletcherResponse = await FletcherResponse.json();
   } catch (error) {
-      console.error(`jsFetch error: ${error}`);
+      console.error(`Fletcher error: ${error}`);
   } finally {
       return FletcherResponse;
   }
